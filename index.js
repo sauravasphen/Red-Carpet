2e8f8de379cabc61c56ca71b33ab015457f96a25
sauravasphen <75376341+sauravasphen@users.noreply.github.com>
2023-01-16 08:08:27 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// ------------- SlideShow ----------------
+
+// const slides = document.querySelectorAll(".slide");
+// console.log(slides);
+// var count = 0;
+// slides.forEach((slide, index) => {
+//   slide.style.left = `${index * 100}%`;
+// });
+// function slideImage() {
+//   slides.forEach((slide) => {
+//     slide.style.transform = `translateX(-${count * 100}%)`;
+//   });
+// }
+// function goPrev() {
+//   count--;
+//   slideImage();
+// }
+// function goNext() {
+//   count++;
+//   slideImage();
+// }
+
+//  ------ Easy Option -------------
+let slideIndex = 0;
+setInterval(showSlides,4000);
+
+function showSlides() {
+  let i;
+  let slides = document.querySelectorAll(".slide");
+//   let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+
+  slides[slideIndex-1].style.display = "block";  
+//   setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+// ------------ Change search icon on hover -> Search box --------
+const searchIcon = document.getElementById("search-icon");
+console.log(searchIcon)
+function hover() {
+    searchIcon.src ='./media/search-white.png';
+  }
+  
+  function unhover() {
+    searchIcon.src = './media/search-red.png';
+  }
